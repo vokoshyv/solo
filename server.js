73ea00449d4134e7405feb58f2d2e4b36c5883d9
vokoshyv <ronsuij@gmail.com>
2015-04-27 21:10:14 -0700

    right before deploying to azure

---
+++ server.js
@@ -0,0 +1,21 @@
+var app = require('./server-config.js');
+var express = require('express');
+
+// var io = require('socket.io')(app);
+
+// var port = (process.env.PORT)? process.env.PORT : 0000;
+var port = 8000;
+
+app.listen(port, function(){
+  console.log('Listening to port ', port);
+});
+
+app.use(express.static('public'));
+
+
+// io.on('connection', function (socket) {
+//   socket.emit('news', { hello: 'world' });
+//   socket.on('my other event', function (data) {
+//     console.log(data);
+//   });
+// });
