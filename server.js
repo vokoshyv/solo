27e43a720ed6857d4b771432630cc523abbfda14
vokoshyv <ronsuij@gmail.com>
2015-04-28 17:51:56 -0700

    final deploy before presentation

--- server.js
+++ server.js
@@ -1,7 +1,27 @@
 var app = require('./server-config.js');
 var express = require('express');
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 // var io = require('socket.io')(app);
+
+// app.get('/', function(req, res){
+//   res.sendFile(__dirname + '/client/chat.html');
+// });
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  })
+});
+
+io.on('disconnect', function(){
+  console.log('user disconnected');
+})
+
+// http.listen(3000, function(){
+//   console.log('listening on *: 3000');
+// });
 
 // var port = (process.env.PORT)? process.env.PORT : 0000;
 var port = 8000;
